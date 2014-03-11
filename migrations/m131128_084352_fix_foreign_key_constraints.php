a04301d64f7b0575854f02b55c400d7bd42b2e5f
Diego Gullo <diego_gullo@bizmate.biz>
2014-03-11 16:22:13 +0000

    TestData - introduce test data in the last migration run in testdata.sql for Correspondance

--- migrations/m131128_084352_fix_foreign_key_constraints.php
+++ migrations/m131128_084352_fix_foreign_key_constraints.php
@@ -1,6 +1,6 @@
 <?php
 
-class m131128_084352_fix_foreign_key_constraints extends CDbMigration
+class m131128_084352_fix_foreign_key_constraints extends OEMigration
 {
 	public $tables = array(
 		'et_ophcocorrespondence_firm_letter_macro' => 'et_ophcocorrespondence_flm',
@@ -20,6 +20,13 @@ class m131128_084352_fix_foreign_key_constraints extends CDbMigration
 			$this->addForeignKey("{$prefix}_created_user_id_fk",$table,"created_user_id","user","id");
 			$this->addForeignKey("{$prefix}_last_modified_user_id_fk",$table,"last_modified_user_id","user","id");
 		}
+
+		$this->execute("SET foreign_key_checks = 0");
+		Yii::app()->cache->flush();
+		$migrations_path = dirname(__FILE__);
+		$this->initialiseData($migrations_path);
+		//enable foreign keys check
+		$this->execute("SET foreign_key_checks = 1");
 	}
 
 	public function down()
